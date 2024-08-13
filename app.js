942f77583bcb9c502e610f0695ef00bdb59318e3
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-13 18:13:01 +0300

    fetch added

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const searchInput = document.querySelector(".form-control");
 
-
+const verileriGetir = async () => {
+  const URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+  try {
+    const res = await fetch(URL);
+    if (!res.ok) {
+      throw new Error(`Hata kodu: ${res.status}`);
+    }
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};

df69e95e29b25ec1679ee3184a84be536f25cb34
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-13 18:42:58 +0300

    finish projects

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const searchInput = document.querySelector(".form-control");
+const searchInput = document.querySelector("input");
+const cardDiv = document.querySelector(".cardDiv");
 
 const verileriGetir = async () => {
-  const URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput.value}`;
   try {
     const res = await fetch(URL);
     if (!res.ok) {
       throw new Error(`Hata kodu: ${res.status}`);
     }
     const data = await res.json();
-    console.log(data);
+    search(data);
   } catch (error) {
     console.log(error);
   }
 };
+
+function search(data) {
+  cardDiv.innerHTML = "";
+  data.meals.forEach((meal) => {
+    cardDiv.innerHTML = `
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions}</p>
+            </div>
+        </div>
+        `;
+  });
+}
+searchInput.addEventListener("input", verileriGetir);
